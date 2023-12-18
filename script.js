7d5feb774c8c1930c110f78c7606d144d45dd54a
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-12-18 14:29:43 +0600

    scroll animation added

--- script.js
+++ script.js
@@ -6,10 +6,31 @@ backgroundColor:"black",
         trigger:"#nav",
         scroller:"body",
         markers: true,
-        start:"top-10%",
+        start:"top -10%",
         end:"top -10%",
         scrub:1
 
     }
     
 }) 
+
+
+// cursor animation
+
+document.addEventListener("mousemove",function (dets){
+    console.log(dets);
+})
+
+
+
+gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+    }
+})
