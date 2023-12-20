5d3153a36abc51de351ce658abebaeb640614f59
Ziauddin Muntasir Chowdhury <ziauddinmuntasir587@gmail.com>
2023-12-20 21:19:21 +0600

    about section animation added

--- script.js
+++ script.js
@@ -19,11 +19,29 @@ backgroundColor:"black",
 let cursorpoint=document.querySelector("#cursor")
 let cursorpointblur=document.querySelector("#cursor-blur")
 document.addEventListener("mousemove",function (dets){
-    cursorpoint.style.left=dets.x+"px"
-    cursorpoint.style.top=dets.y+"px"
+    cursorpoint.style.left=dets.x+2+"px"
+    cursorpoint.style.top=dets.y-3+"px"
     cursorpointblur.style.left=dets.x-150+"px"
     cursorpointblur.style.top=dets.y-150+"px"
 
+})
+
+
+const h4all=document.querySelectorAll('#nav h4')
+
+h4all.forEach(function(elem){
+   elem.addEventListener("mouseenter", function(){
+    cursorpoint.style.scale=2
+    cursorpoint.style.border="1px solid #fff"
+    cursorpoint.style.backgroundColor="transparent"
+
+   })
+   elem.addEventListener("mouseleave", function(){
+    cursorpoint.style.scale=1
+    cursorpoint.style.border="0px"
+    cursorpoint.style.backgroundColor="#95c11e"
+    
+   })
 })
 
 
@@ -37,5 +55,23 @@ gsap.to("#main",{
         start:"top -25%",
         end:"top -70%",
         scrub:2
+    }
+})
+
+
+
+gsap.from("#aboutus img,#about-us-title ",{
+
+    y:50,
+    opacity:0,
+    duration:1,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:"#about-us-title",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+        end:"top 55%",
+        scrub:3
     }
 })
